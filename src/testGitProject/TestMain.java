324030445888ddd22cebc264f414c8e74f9d4ac7
wpf104533@alibaba-inc.com <wpf104533@alibaba-inc.com>
2016-01-19 15:16:10 +0800

    java project

---
+++ src/testGitProject/TestMain.java
@@ -0,0 +1,10 @@
+package testGitProject;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java project one is coming!");
+	}
+
+}
